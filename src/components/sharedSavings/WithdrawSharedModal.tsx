e8a70237a71e9d455c0775a2c4e46bb0b47b5e10
Azzzull <azulkarnaen7@gmail.com>
2026-09-08 11:44:06 +0700

    fix error "Match the requested format" di Withdrawal shared savings

--- src/components/sharedSavings/WithdrawSharedModal.tsx
+++ src/components/sharedSavings/WithdrawSharedModal.tsx
@@ -161,7 +161,6 @@ export function WithdrawSharedModal({
             autoFocus
             placeholder="0"
             inputMode="numeric"
-            pattern="[0-9]*"
             hint={t("shared.withdrawAmountMax", { amount: formatCurrency(myAvailableShare, "IDR") })}
             value={formatMoneyDigits(amountDigits)}
             onChange={(e) => {
